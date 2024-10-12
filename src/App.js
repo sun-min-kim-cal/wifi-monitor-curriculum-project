730b3b5f5efa30434454e717660826c548535bc4
Melody <melodyjcui@gmail.com>
2024-10-11 17:58:15 -0700

    new

--- src/App.js
+++ src/App.js
@@ -15,15 +15,22 @@ import {
 function App() {
   return (
     <div className="App">
-      <div className="leaflet-container">
+      <div style={{
+        height: '70vh',
+        marginBottom: '1rem',
+        borderRadius: '1.5rem',
+        borderStyle: 'solid',
+        borderWidth: 'thin',
+        borderColor: '#25B4E7'
+      }}>
         <MapContainer center={[37.8719, -122.2585]} zoom={17} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
         </MapContainer>
-      </div>
     </div>
+  </div>
   );
 }
 
