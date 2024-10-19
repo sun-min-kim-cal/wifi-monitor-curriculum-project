3452557c391196b5ae04bbc3c107043607cb92e0
Sun Min Kim <sun_min.kim@berkeley.edu>
2024-10-18 17:48:32 -0700

    Implemented basic layout for boxes

--- src/App.js
+++ src/App.js
@@ -51,31 +51,57 @@ function App() {
             borderRadius: 1,
           }}
         >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: 3/10,
+              p: 1,
+              m: 1,
+              bgcolor: 'background.paper',
+              borderRadius: 1,
+            }}
+          >
+          <p>Where are you located?</p>
           <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+            <InputLabel id="demo-simple-select-label">Select current location</InputLabel>
             <Select
               labelId="demo-simple-select-label"
               id="demo-simple-select"
               label="Age"
             >
               <MenuItem value={10}>MLK</MenuItem>
-              <MenuItem value={20}>not MLK</MenuItem>
-              <MenuItem value={30}>not MLK</MenuItem>
+              <MenuItem value={20}>Moffitt</MenuItem>
+              <MenuItem value={30}>MainStacks</MenuItem>
             </Select>
           </FormControl>
+          </Box>
 
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: 7/10,
+              p: 1,
+              m: 1,
+              bgcolor: 'background.paper',
+              borderRadius: 1,
+            }}
+          >
+          <p>How is the campus wifi at your current location?</p>
           <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+            <InputLabel id="demo-simple-select-label">Select your response</InputLabel>
             <Select
               labelId="demo-simple-select-label"
               id="demo-simple-select"
               label="Age"
             >
-              <MenuItem value={10}>MLK</MenuItem>
-              <MenuItem value={20}>not MLK</MenuItem>
-              <MenuItem value={30}>not MLK</MenuItem>
+              <MenuItem value={10}>Stable</MenuItem>
+              <MenuItem value={20}>Unstable</MenuItem>
+              <MenuItem value={30}>Non-connectable</MenuItem>
             </Select>
           </FormControl>
+          </Box>
         </Box>
         <Box
           sx={{
@@ -87,11 +113,45 @@ function App() {
             borderRadius: 1,
           }}
         > 
-          <Button variant="outlined">RESET LOCATION</Button>
-
-          <Button variant="outlined">Submit Feedback</Button>
-
-          <Button variant="outlined">Clear Response</Button>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              width: 1/3,
+              p: 1,
+              m: 1,
+              bgcolor: 'background.paper',
+              borderRadius: 1,
+            }}
+          > 
+            <Button variant="outlined">RESET LOCATION</Button>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              width: 1/3,
+              p: 1,
+              m: 1,
+              bgcolor: 'background.paper',
+              borderRadius: 1,
+            }}
+          > 
+            <Button variant="outlined">Submit Feedback</Button>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              width: 1/3,
+              p: 1,
+              m: 1,
+              bgcolor: 'background.paper',
+              borderRadius: 1,
+            }}
+          > 
+            <Button variant="outlined">Clear Response</Button>
+          </Box>
         </Box>
       </div>
     </div>
